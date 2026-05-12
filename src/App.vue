0f678088ccde032a56f645061bcc3f8dc66d0665
andreialex232 <numauitnuintelegi@gmail.com>
2026-05-12 19:52:51 +0200

    added tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="min-h-screen bg-gray-100 flex items-center justify-center">
+    <h1 class="text-4xl font-bold text-blue-600 underline">
+      Tailwind is working!
+    </h1>
+  </div>
 </template>
 
 <style scoped></style>
