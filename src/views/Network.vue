501c2d4b9a374e331b7c74b314e596597311c5f1
andreialex232 <numauitnuintelegi@gmail.com>
2026-05-17 05:35:15 +0200

    wip network view

---
+++ src/views/Network.vue
@@ -0,0 +1,13 @@
+<script setup>
+    import HeroNetwork from '@/components/network-components/Hero-network.vue';
+    import TypesOfNetwork from '@/components/network-components/Types-of-network.vue';
+    import AddOrganization from '@/components/network-components/Add-organization.vue';
+</script>
+
+<template>
+    <div>
+        <HeroNetwork></HeroNetwork>
+        <TypesOfNetwork></TypesOfNetwork>
+        <AddOrganization></AddOrganization>
+    </div>
+</template>
