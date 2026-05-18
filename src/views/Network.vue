8bc30be25448916677ee8482e903af68eda3e896
andreialex232 <numauitnuintelegi@gmail.com>
2026-05-19 01:32:55 +0200

    added network actors page

--- src/views/Network.vue
+++ src/views/Network.vue
@@ -2,12 +2,14 @@
     import HeroNetwork from '@/components/network-components/Hero-network.vue';
     import TypesOfNetwork from '@/components/network-components/Types-of-network.vue';
     import AddOrganization from '@/components/network-components/Add-organization.vue';
+    import NetworkActors from '@/components/network-components/Network-actors.vue';
 </script>
 
 <template>
     <div>
         <HeroNetwork></HeroNetwork>
         <TypesOfNetwork></TypesOfNetwork>
+        <NetworkActors></NetworkActors>
         <AddOrganization></AddOrganization>
     </div>
 </template>
