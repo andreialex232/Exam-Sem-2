45ff4d2c0d52b3db2d1d65e4ad6b14f30f021554
andreialex232 <numauitnuintelegi@gmail.com>
2026-05-22 02:23:22 +0200

    dynamic routing for network unit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
-import Events from '@/views/Events.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,6 +17,12 @@ const router = createRouter({
     path: '/events',
     name: 'events',
     component: () => import('@/views/Events.vue')
+  },
+  {
+    path: '/network/:name',
+    name: 'network-profile',
+    component: () => import('@/components/network-components/Network-profile.vue'),
+    props: true
   }
   ]
 })
