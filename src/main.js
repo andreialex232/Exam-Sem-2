cfc05dfa50e42c076773d712a3bd392776012b69
andreialex232 <numauitnuintelegi@gmail.com>
2026-05-26 14:20:07 +0200

    added i18n

--- src/main.js
+++ src/main.js
@@ -2,9 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './style.css'
+import { createI18n } from 'vue-i18n'
 
+
+const i18n = createI18n({
+    legacy: false,
+    locale: "en",
+    fallbackLocale: "en"
+})
 const app = createApp(App)
 
 app.use(router)
-
+app.use(i18n)
 app.mount('#app')
