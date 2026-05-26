4c753a81469ff4fc2e0bb73b28e1ea694ddf439b
andreialex232 <numauitnuintelegi@gmail.com>
2026-05-27 00:44:03 +0200

    added events search section

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -4,11 +4,13 @@ import EventTypeSection from '@/components/events-components/event-type-section.
 import PastEvents from '@/components/events-components/past-events.vue';
 import EventUpdates from '@/components/events-components/event-updates.vue';
 import EventFindPeople from '@/components/events-components/events-findPeople.vue';
+import EventsActors from '@/components/events-components/events-actors.vue';
 </script>
 
 <template>
   <EventsHero />
   <EventTypeSection />
+  <EventsActors></EventsActors>
   <PastEvents />
   <EventUpdates />
   <EventFindPeople />
