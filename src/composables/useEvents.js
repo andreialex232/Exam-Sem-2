ab776b06d5596f11a8bfaad13c4fcf481134c48c
andreialex232 <numauitnuintelegi@gmail.com>
2026-05-27 00:43:51 +0200

    composable for events

---
+++ src/composables/useEvents.js
@@ -0,0 +1,162 @@
+import { ref } from "vue";
+
+export function useEvents() {
+    const events = ref([
+        {
+            id: "1",
+            slug: "expert-interview",
+            name: "Get Experience in Interviewing",
+            location: "Kolding, Denmark",
+            language: ["DA / EN"],
+            date: {
+                day: "12",
+                time: "june"
+            },
+            description: "Helps companies understand recruitment and onboarding across the border.",
+            language: ["da", "en"],
+            role: "interview",
+        },
+        {
+            id: "2",
+            slug: "expert-workshop",
+            name: "Get Experience in Workshops",
+            location: "Kolding, Denmark",
+            language: ["DA / EN"],
+            date: {
+                day: "20",
+                time: "june"
+            },
+            description: "Helps companies understand recruitment and onboarding across the border.",
+            language: ["da", "en"],
+            role: "workshop",
+        },
+        {
+            id: "3",
+            slug: "expert-workshop",
+            name: "expert-workshop",
+            location: "Kolding, Denmark",
+            language: ["DA / EN"],
+            date: {
+                day: "20",
+                time: "june"
+            },
+            description: "Helps companies understand recruitment and onboarding across the border.",
+            language: ["da", "en"],
+            role: "workshop",
+        },
+        {
+            id: "4",
+            slug: "expert-workshop",
+            name: "expert-workshop",
+            location: "Kolding, Denmark",
+            language: ["DA / EN"],
+            date: {
+                day: "20",
+                time: "june"
+            },
+            description: "Helps companies understand recruitment and onboarding across the border.",
+            language: ["da", "en"],
+            role: "workshop",
+        },
+        {
+            id: "5",
+            slug: "expert-workshop",
+            name: "expert-workshop",
+            location: "Kolding, Denmark",
+            language: ["DA / EN"],
+            date: {
+                day: "20",
+                time: "june"
+            },
+            description: "Helps companies understand recruitment and onboarding across the border.",
+            language: ["da", "en"],
+            role: "workshop",
+        },
+        {
+            id: "6",
+            slug: "expert-workshop",
+            name: "expert-workshop",
+            location: "Kolding, Denmark",
+            language: ["DA / EN"],
+            date: {
+                day: "20",
+                time: "june"
+            },
+            description: "Helps companies understand recruitment and onboarding across the border.",
+            language: ["da", "en"],
+            role: "workshop",
+        },
+        {
+            id: "7",
+            slug: "expert-workshop",
+            name: "expert-workshop",
+            location: "Kolding, Denmark",
+            language: ["DA / EN"],
+            date: {
+                day: "20",
+                time: "june"
+            },
+            description: "Helps companies understand recruitment and onboarding across the border.",
+            language: ["da", "en"],
+            role: "workshop",
+        },
+        {
+            id: "8",
+            slug: "expert-workshop",
+            name: "expert-workshop",
+            location: "Kolding, Denmark",
+            language: ["DA / EN"],
+            date: {
+                day: "20",
+                time: "june"
+            },
+            description: "Helps companies understand recruitment and onboarding across the border.",
+            language: ["da", "en"],
+            role: "workshop",
+        },
+        {
+            id: "9",
+            slug: "expert-workshop",
+            name: "expert-workshop",
+            location: "Kolding, Denmark",
+            language: ["DA / EN"],
+            date: {
+                day: "20",
+                time: "june"
+            },
+            description: "Helps companies understand recruitment and onboarding across the border.",
+            language: ["da", "en"],
+            role: "workshop",
+        },
+        {
+            id: "10",
+            slug: "expert-workshop",
+            name: "expert-workshop",
+            location: "Kolding, Denmark",
+            language: ["DA / EN"],
+            date: {
+                day: "20",
+                time: "june"
+            },
+            description: "Helps companies understand recruitment and onboarding across the border.",
+            language: ["da", "en"],
+            role: "workshop",
+        },
+        {
+            id: "11",
+            slug: "expert-workshop",
+            name: "expert-workshop",
+            location: "Kolding, Denmark",
+            language: ["DA / EN"],
+            date: {
+                day: "20",
+                time: "june"
+            },
+            description: "Helps companies understand recruitment and onboarding across the border.",
+            language: ["da", "en"],
+            role: "workshop",
+        },
+    ])
+
+    return { events };
+}
