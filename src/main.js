f3eba2c6294ce2b36e050ea89f81af9dd98aa436
andreialex232 <numauitnuintelegi@gmail.com>
2026-05-28 11:24:44 +0200

    added vue unhead

--- src/main.js
+++ src/main.js
@@ -1,26 +1,27 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import './style.css'
+import { createHead } from '@unhead/vue/client'
 import { createI18n } from 'vue-i18n'
+import './style.css'
 
 import en from './locales/en.json'
 import de from './locales/de.json'
 import da from './locales/da.json'
 
+const head = createHead()
+
 const i18n = createI18n({
-    legacy: false,
-    locale: localStorage.getItem('user-locale') || 'en', // 
-    fallbackLocale: 'en',
-    messages: {
-        en,
-        de,
-        da
-    }
+  legacy: false,
+  locale: localStorage.getItem('user-locale') || 'en', 
+  fallbackLocale: 'en',
+  messages: { en, de, da }
 })
 
 const app = createApp(App)
 
 app.use(router)
+app.use(head)
 app.use(i18n)
+
 app.mount('#app')
